266ab996a0b75a421325a03ae84cafdb7b7753bd
surachat-sudo <surachai27999@gmail.com>
2026-02-17 15:44:28 +0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PM-Office/',   // 👈 ชื่อ repo ต้องตรง
+  base: '/PM-Office/',  // 👈 ชื่อ repo มึง
 })
